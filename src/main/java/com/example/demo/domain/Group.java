497fed1fc39b052632eeca196801cf56f5ad0e75
prannoy29 <prannoysarkar1993@gmail.com>
2017-10-15 22:04:06 +0530

    Added Group fields

--- src/main/java/com/example/demo/domain/Group.java
+++ src/main/java/com/example/demo/domain/Group.java
@@ -1,4 +1,65 @@
-package main.java.com.example.demo.domain;
+package com.example.demo.domain;
 
-public class Group {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "group")
+public class Group implements Serializable {
+
+    private static final long serialVersionUID = 34567898l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "groupId")
+    private long groupId;
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    @Column(name = "groupName",nullable = false)
+    private String groupName;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Column(name = "createdBy",nullable = false)
+    private String createdBy;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Column(name = "createdAt",nullable = false)
+    private String createdAt;
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    protected Group(){}
+
+    public Group(String groupName, String createdBy, String createdAt) {
+        this.groupName = groupName;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+    }
 }
